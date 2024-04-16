fb0401762b373268772cb4d8c15bd55e8c574966
drewfowles <157662178+drewfowles@users.noreply.github.com>
2024-04-16 11:40:16 -0600

    Create cars.py

---
+++ cars.py
@@ -0,0 +1,53 @@
+# Problem 1
+print("Drew Fowles")
+
+# Problem 2
+cars = ['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
+
+# Problem 3
+print(cars)
+
+# Problem 4
+print(len(cars))
+
+# Problem 5
+cars.append('Buick')
+
+# Problem 6
+print(cars)
+
+# Problem 7
+print(cars[3])
+
+# Problem 8
+cars.insert(3, 'Toyota')
+
+# Problem 9
+print(cars)
+
+# Problem 10
+cars.pop(4)
+
+# Problem 11
+print(cars)
+
+# Problem 12
+cars.sort()
+
+# Problem 13
+print(cars)
+
+# Problem 14
+cars.sort(reverse=True)
+
+# Problem 15
+print(cars)
+
+# Problem 16
+my_array_length = len(cars)
+
+# Problem 17
+array_string = 'The length of my array is '
+
+# Problem 18
+print(array_string + str(my_array_length))
